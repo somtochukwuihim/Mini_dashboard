149c4363be89dcf3d0bf4e7fa7c4e827f5b83cc1
Somtochukwu Ihim <ihimsomtochukwu@gmail.com>
2026-09-11 12:21:48 +0100

    Added a single counter function

--- mini_dashboard.py
+++ mini_dashboard.py
@@ -1,32 +1,17 @@
 import json
 import os
 
-#////////CHANGES MADE-[Code n+2]//////////
-# i made the code tie sensitive.
-# it can now return more than one customer if there is a tie
+#////////CHANGES MADE-[Code n+3]//////////
+# created the counter function to:
+# loop through the tickets and compare: key to matching value condition
+# Avoid repetitive code.
 
-def count_open_tickets (tickets ):
-    open_count = 0
+def counter (tickets, key, value):
+    count = 0
     for ticket in tickets:
-        if ticket['status'] == 'open':
-            open_count +=1
-    return open_count
-            
-def count_closed_tickets(tickets):
-    closed_count = 0
-    for ticket in tickets:
-        status = ticket['status']
-        if status == 'closed':
-            closed_count += 1
-    return closed_count
-        
-def count_high_priority (tickets):
-    high_priority = 0
-    for ticket in tickets:
-        priority = ticket['priority']
-        if priority == 'high':
-            high_priority += 1
-    return high_priority
+        if ticket.get(key, 'misising') == value:
+            count += 1
+    return count
 
 def customer_with_most_open_ticket (tickets):
     most_open_ticket = {}
@@ -41,7 +26,7 @@ def customer_with_most_open_ticket (tickets):
     for customer, count in most_open_ticket.items():
         if count > highest_count:
             highest_count = count
-            winner =[customer]
+            winner = [customer]
             
         elif count == highest_count:
            winner.append(customer)
@@ -51,9 +36,11 @@ def customer_with_most_open_ticket (tickets):
 
 def print_dashboard (tickets=None):
     if tickets is None:
-        return 'Error: No file/file-path was provided to the function.'    
+        return 'Error: No file/file-path was provided to the function.'
+    
     if not os.path.exists(tickets):
-        raise FileNotFoundError ('Error: File does not exist')     
+        raise FileNotFoundError ('Error: File does not exist') 
+    
     if os.path.isdir(tickets):
         return 'File only points to a directory'
     
@@ -62,9 +49,9 @@ def print_dashboard (tickets=None):
             ticket_holder =  json.load(f)
             tickets = ticket_holder
 
-            open_ticket = count_open_tickets (tickets)
-            closed_tickets = count_closed_tickets (tickets)
-            high_priority_tickets = count_high_priority (tickets)
+            open_ticket = counter (tickets, 'status', 'open')
+            closed_tickets = counter (tickets, 'status', 'closed')
+            high_priority_tickets = counter (tickets, 'priority', 'high')
             customer, count = customer_with_most_open_ticket (tickets)
             print ('=========SUPPORT DASHBOARD===========\n')
             print (f'Open Tickets: {open_ticket}\n')
